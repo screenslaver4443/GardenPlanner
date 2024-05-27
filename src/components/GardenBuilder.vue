3abab25a1762825efc475c5d880e292f6ccddabe
Nikolai <57935446+screenslaver4443@users.noreply.github.com>
2024-05-27 15:20:11 +0930

    Removed Included pages and componets, handling plant Json

---
+++ src/components/GardenBuilder.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Gardening World</h1>
+</template>
+<script>
+import jsonData from "@/assets/plants.json";
+export default {
+    data() {
+        return {
+            plantJson: jsonData,
+            areaById: [],
+            nameById: [],
+        }
+    },
+    methods: {
+    },
+    props: {
+    },
+    created() {
+        for (var i = 0; i < this.plantJson.length; i++) {
+            this.areaById.push({ key: this.plantJson[i].id, value: this.plantJson[i].minArea })
+            this.nameById.push({ key: this.plantJson[i].id, value: this.plantJson[i].plant })
+        }
+    },
+
+}
+</script>
+
+
+<style></style>
